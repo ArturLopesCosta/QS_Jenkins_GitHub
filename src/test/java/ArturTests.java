69a825de2230f865ef1a8abbcdc12257e91fe7b4
ArturLopesCosta <artur.costa@sapo.pt>
2016-05-31 00:04:57 +0100

    Adicionar logo escolas e testar de imagens são mostradas

--- src/test/java/ArturTests.java
+++ src/test/java/ArturTests.java
@@ -1,13 +1,17 @@
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,10 +29,11 @@ public class ArturTests {
 
     @Before
     public void setUp() throws Exception {
-        driver = new HtmlUnitDriver();
+        driver = new FirefoxDriver();
+        //driver = new HtmlUnitDriver();
         //http://www.arturcosta.com/qs/
         baseUrl = "http://www.arturcosta.com/qs/";
-        //baseUrl = "http://localhost:8080/team/";
+        //baseUrl = "http://localhost:8080/team";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     //
@@ -39,6 +44,7 @@ public class ArturTests {
         //Assert.assertTrue("erro no link",driver.getTitle().matches(""));
         //Assert.assertEquals("Wrong page title","ALC Computer - Hardware, Software, Assistência Técnica, Redes, Web Design e Cartões PVC | Página Inicial",driver.getTitle());
         driver.get(baseUrl + "/arturcosta.html");
+
         driver.findElement(By.linkText("Site ALC Computer")).click();
         Assert.assertEquals("ALC Computer - Hardware, Software, Assistência Técnica, Redes, Web Design e Cartões PVC | Página Inicial",driver.getTitle());
 
@@ -57,12 +63,39 @@ public class ArturTests {
         //driver.findElement(By.linkText("Formador")).click();
         //Assert.assertEquals("ISLA Santarém",driver.getTitle());
 
+        driver.get(baseUrl + "/arturcosta.html");
+        WebElement image1 = driver.findElement(By.xpath("//img[@alt='Logo IPL']"));
+      //  WebElement image1 = driver.findElement(By..xpath("http[contains(@alt,'Logo IPL')]"));
+       // Boolean imageLoaded1 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != "undefined" && arguments[0].naturalWidth > 0", image1);
+        Boolean imageLoaded1 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image1);
+       /* if (!imageLoaded1)
+        {
+            System.out.println("1. Image is not present");
+        }
+        else
+        {
+            System.out.println("1. Got it");
+        }*/
+        Assert.assertEquals(true, imageLoaded1);
+        WebElement image2 = driver.findElement(By.xpath("//img[@alt='Logo ESGTS']"));
+
+        Boolean imageLoaded2 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image2);
+       /* if (!imageLoaded2)
+        {
+            System.out.println("2. Image is not present");
+        }
+        else
+        {
+            System.out.println("2. Got it");
+        }*/
+
+        Assert.assertEquals(true, imageLoaded2);
+        driver.close();
     }
 
     @After
     public void tearDown() throws Exception {
         driver.quit();
-
     }
 }
 
