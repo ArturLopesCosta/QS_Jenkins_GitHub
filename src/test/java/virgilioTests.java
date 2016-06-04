1bdad917aed0e6a32e79be9e03d07a7190c832fa
Virgilio Manuel Mateus dos Reis <virgiliomanuelr@gmail.com>
2016-06-04 17:23:17 +0100

    Test only for the url facebook Test if the url facebook is a facebook page

--- src/test/java/virgilioTests.java
+++ src/test/java/virgilioTests.java
@@ -15,6 +15,8 @@ public class virgilioTests {
     //private HtmlUnitDriver driver;
     private String baseUrl;
     private String searchTerm;
+    private String personalPage = "VirgilioReis.html";
+    private String personalURL ;
 
 
 
@@ -28,12 +30,13 @@ public class virgilioTests {
         //http://www.arturcosta.com/qs/
         baseUrl = "http://www.arturcosta.com/qs/"; //baseUrl for the online version
         //baseUrl = "http://localhost:8080/team/"; //baseUrl for the local version
+        personalURL = baseUrl+personalPage;
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     //
     @Test
     public void testJavaTeste() throws Exception {
-        driver.get(baseUrl + "/");
+        driver.get(baseUrl);
         //driver.findElement(By.linkText("Web page profissional")).click();
         //Assert.assertTrue("erro no link",driver.getTitle().matches(""));
         //Assert.assertEquals("Wrong page title","ALC Computer - Hardware, Software, Assistência Técnica, Redes, Web Design e Cartões PVC | Página Inicial",driver.getTitle());
@@ -61,7 +64,28 @@ public class virgilioTests {
 
         Assert.assertEquals(true, imageLoaded2);
 
+
+
+    }
+
+    @Test
+    public void testFacebookLink() throws Exception {
+
+
+
+        driver.get(personalURL);
+        Assert.assertEquals("http://localhost:8080/team/VirgilioReis.html", driver.getCurrentUrl());
+        Assert.assertEquals("Virgilio Reis", driver.getTitle());
+
+        //Find and click the url facebook on the personal webpage
         driver.findElement(By.linkText("My Facebook")).click();
+        //Check if the page opened it a facebook page
+        Assert.assertEquals("Facebook", driver.getTitle());
+        //Check if the url page is correct
+        Assert.assertEquals("https://www.facebook.com/virgilio.reis.7", driver.getCurrentUrl());
+        //Check if the url bellong to the Virgilio Reis facebook web page
+        //*[@id="fb-timeline-cover-name"]
+        //Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"fb-timeline-cover-name\"]")).getText(), "Virgilio Reis");
 
     }
 
