7a272e2530e9b5ddd397e3ab35049351f58e2dca
ArturLopesCosta <artur.costa@sapo.pt>
2016-06-06 00:33:02 +0100

    Organização do Site e Testes

--- src/test/java/ArturTests.java
+++ src/test/java/ArturTests.java
@@ -31,26 +31,24 @@ public class ArturTests {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         //driver = new HtmlUnitDriver();
-        //http://www.arturcosta.com/qs/
+        http://www.arturcosta.com/qs/
         baseUrl = "http://www.arturcosta.com/qs/";
-        //baseUrl = "http://localhost:8080/team";
+        //baseUrl = "http://localhost:8080/team/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     //
+
     @Test
     public void testJavaTeste() throws Exception {
-
-        //driver.findElement(By.linkText("Web page profissional")).click();
-        //Assert.assertTrue("erro no link",driver.getTitle().matches(""));
-        //Assert.assertEquals("Wrong page title","ALC Computer - Hardware, Software, Assistência Técnica, Redes, Web Design e Cartões PVC | Página Inicial",driver.getTitle());
+        //página inicial do perfil
         driver.get(baseUrl + "/arturcosta.html");
-        WebElement fotoArtur = driver.findElement(By.xpath("//img[@alt='Foto Artur']"));
-         Boolean fotoCarregada = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", fotoArtur);
+        Assert.assertEquals("Artur Costa",driver.getTitle());
+        driver.close();
+    }
+    @Test
+    public void testLinks() throws Exception {
 
-        Assert.assertEquals(true, fotoCarregada);
-
-       // WebElement fotoArtur = driver.findElement(By.xpath("//img[contains(@id,'fotoArtur')]"));
-        //  Assert.assertEquals(true, fotoArtur.isEnabled());
+        driver.get(baseUrl + "/arturcosta.html");
 
         driver.findElement(By.linkText("Site ALC Computer")).click();
         Assert.assertEquals("ALC Computer - Hardware, Software, Assistência Técnica, Redes, Web Design e Cartões PVC | Página Inicial",driver.getTitle());
@@ -66,39 +64,43 @@ public class ArturTests {
         driver.get(baseUrl + "/arturcosta.html");
         driver.findElement(By.linkText("Minhaventura")).click();
         Assert.assertEquals("descer o rio Coura em kayak - MinhAventura",driver.getTitle());
+        driver.close();
 
-        //driver.findElement(By.linkText("Formador")).click();
-        //Assert.assertEquals("ISLA Santarém",driver.getTitle());
-
+    }
+    @Test
+    public void testImagens() throws Exception {
+        //foto pessoal
+        driver.get(baseUrl + "/arturcosta.html");
+        WebElement fotoArtur = driver.findElement(By.xpath("//img[@alt='Foto Artur']"));
+        Boolean fotoCarregada = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", fotoArtur);
+        Assert.assertEquals(true, fotoCarregada);
+        //logo IPL
         driver.get(baseUrl + "/arturcosta.html");
         WebElement image1 = driver.findElement(By.xpath("//img[@alt='Logo IPL']"));
-      //  WebElement image1 = driver.findElement(By..xpath("http[contains(@alt,'Logo IPL')]"));
-       // Boolean imageLoaded1 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != "undefined" && arguments[0].naturalWidth > 0", image1);
         Boolean imageLoaded1 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image1);
-       /* if (!imageLoaded1)
-        {
-            System.out.println("1. Image is not present");
-        }
-        else
-        {
-            System.out.println("1. Got it");
-        }*/
         Assert.assertEquals(true, imageLoaded1);
+        //Logo ESGTS
         WebElement image2 = driver.findElement(By.xpath("//img[@alt='Logo ESGTS']"));
-
         Boolean imageLoaded2 = (Boolean) ((JavascriptExecutor)driver).executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", image2);
-       /* if (!imageLoaded2)
-        {
-            System.out.println("2. Image is not present");
-        }
-        else
-        {
-            System.out.println("2. Got it");
-        }*/
-
         Assert.assertEquals(true, imageLoaded2);
+
         driver.close();
+
     }
+    @Test
+    public void testVideos() throws Exception {
+        //videos Minhaventura
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("Profile Artur Costa")).click();
+        WebElement videoKayak = driver.findElement(By.id("videoKayak"));
+        Assert.assertTrue("video videoKayak não está presente",videoKayak.isDisplayed());
+        WebElement videoChallenger = driver.findElement(By.id("videoKayak"));
+        Assert.assertTrue("video videoChallenger não está presente",videoChallenger.isDisplayed());
+
+        driver.close();
+
+    }
+
 
     @After
     public void tearDown() throws Exception {
